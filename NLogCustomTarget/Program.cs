edf4cedee1706e1a2a0c20120fd25f14bc0ad864
snailpjw <alanwei1202@gmail.com>
2021-01-21 09:12:29 +0800

    使用 MethodCall Target, 測試起來似乎不會受 NLog.config設定引響。

--- NLogCustomTarget/Program.cs
+++ NLogCustomTarget/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NLog;
+using NLog.Targets;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,27 @@ using System.Threading.Tasks;
 
 namespace NLogCustomTarget
 {
-    class Program
+    public class Program
     {
+        public static void LogMethod(string level, string message)
+        {
+            Console.WriteLine("l: {0} m: {1}", level, message);
+        }
         static void Main(string[] args)
         {
+            MethodCallTarget target = new MethodCallTarget();
+            target.ClassName = typeof(Program).AssemblyQualifiedName;
+            target.MethodName = "LogMethod";
+            target.Parameters.Add(new MethodCallParameter("${level}"));
+            target.Parameters.Add(new MethodCallParameter("${message}"));
+
+            NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, LogLevel.Debug);
+
+            Logger logger = LogManager.GetLogger("Program");
+            logger.Debug("log message");
+            logger.Error("error message");
+
+            Console.ReadLine();
         }
     }
 }
