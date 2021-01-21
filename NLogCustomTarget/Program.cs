190cbc64662ec82c95c91e7c5c2341f8a6e53e2b
snailpjw <alanwei1202@gmail.com>
2021-01-21 09:40:06 +0800

    測試可以調用NLog.config的正確使用方式。

--- NLogCustomTarget/Program.cs
+++ NLogCustomTarget/Program.cs
@@ -16,17 +16,30 @@ namespace NLogCustomTarget
         }
         static void Main(string[] args)
         {
-            MethodCallTarget target = new MethodCallTarget();
-            target.ClassName = typeof(Program).AssemblyQualifiedName;
-            target.MethodName = "LogMethod";
-            target.Parameters.Add(new MethodCallParameter("${level}"));
-            target.Parameters.Add(new MethodCallParameter("${message}"));
+            //MethodCallTarget target = new MethodCallTarget();
+            //target.ClassName = typeof(Program).AssemblyQualifiedName;
+            //target.MethodName = "LogMethod";
+            //target.Parameters.Add(new MethodCallParameter("${level}"));
+            //target.Parameters.Add(new MethodCallParameter("${message}"));
 
-            NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, LogLevel.Debug);
+            //NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, LogLevel.Debug);
 
-            Logger logger = LogManager.GetLogger("Program");
-            logger.Debug("log message");
-            logger.Error("error message");
+            //Logger logger = LogManager.GetLogger("Program");
+            Logger logger = LogManager.GetCurrentClassLogger();
+
+            //very detailed logs, which may include high-volume information such as protocol payloads.
+            //This log level is typically only enabled during development
+            logger.Trace("Trace");
+            //debugging information, less detailed than trace, typically not enabled in production environment.
+            logger.Debug("Debug");
+            //information messages, which are normally enabled in production environment
+            logger.Info("Info");
+            //warning messages, typically for non-critical issues, which can be recovered or which are temporary failures
+            logger.Warn("Warn");
+            //error messages - most of the time these are Exceptions
+            logger.Error("Error");
+            //very serious errors!
+            logger.Fatal("Fatal");            
 
             Console.ReadLine();
         }
